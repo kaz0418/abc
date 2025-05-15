928ce1cc5b29392fb6632c389a8b5ae82f86ebc4
kaz0418 <tanabekazuki0418@gmail.com>
2025-05-16 08:08:44 +0900

    abc404

--- kaz0418/abc404/c.py
+++ kaz0418/abc404/c.py
@@ -1,14 +1,15 @@
-N,M = map(int,input().split())
+N, M = map(int, input().split())
 G = [[] for i in range(N)]
 
 for _ in range(M):
-    a,b = map(int,input().split())
+    a, b = map(int, input().split())
     G[a-1].append(b-1)
     G[b-1].append(a-1)
 
 visited = [False for i in range(N)]
 
 now = 0
+
 before = -1
 
 visited = []
@@ -17,8 +18,8 @@ for _ in range(N):
     if len(G[now]) != 2:
         print("No")
         exit()
-    
-    a,b = G[now]
+
+    a, b = G[now]
     next = a if before != a else b
 
     visited.append(now)
